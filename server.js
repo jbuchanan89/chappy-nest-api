8b14d68e330ebfedef4f295336a623fe4b477eb9
Jennifer Buchanan <jenniferb.1230@gmail.com>
2018-01-06 13:19:10 -0500

    removed a comma

--- server.js
+++ server.js
@@ -100,30 +100,31 @@ app.get('/api/parent_task/:id/:day', function(req,res){
 	let taskCount = 0;
 
 	console.log('day',req.params.day);
-	let day = req.params.day;
-	console.log(day);
+	let day = req.params.day.split("-");
+
 	User.find({parent_id: req.params.id}, function(err,users){
 		users.forEach(function(user){
 			childrenIDs.push(user._id);
 			children.push(user);
 		});
+
 		// DailyTask.find({child_id: {$in: childrenIDs},day:day[0]+"/"+day[1]+"/"+day[2] },function(err,tasks){
 		// 	taskCount = tasks.length;
 		// 	res.json(tasks);
 		// });
 
-		DailyTask.find({child_id: {$in: childrenIDs},day: day,function(err,tasks){
+		DailyTask.find({child_id: {$in: childrenIDs},day: day},function(err,tasks){
 			taskCount = tasks.length;
 			res.json(tasks);
 		});
 	});
 
-	// function checkComplete(children){
-	// 	count++;
-	// 	if(count == taskCount){
-	// 		res.send(children);
-	// 	}
-	// }
+	function checkComplete(children){
+		count++;
+		if(count == taskCount){
+			res.send(children);
+		}
+	}
 });
 
 // *****************ACCOUNTS****************
